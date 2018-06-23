aef643c6223722d85323fc2ce913018681f173cc
yangxudong <411867545@qq.com>
2018-06-23 17:27:30 +0800

    wireMock伪造服务

--- yxdtyut-security-demo/src/main/java/com/yxdtyut/security/wireMock/MockServer.java
+++ yxdtyut-security-demo/src/main/java/com/yxdtyut/security/wireMock/MockServer.java
@@ -1,7 +1,6 @@
 package com.yxdtyut.security.wireMock;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
@@ -16,14 +15,15 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class MockServer {
     public static void main(String[] args) throws IOException {
-        configureFor(9999);
-        removeAllMappings();
+//        configureFor(9999);
+//        removeAllMappings();
         mock("/order/1","01");
     }
 
     private static void mock(String url, String file) throws IOException {
         ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
-        String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(),"\n");
+        //String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(),"\n");
+        String content = String.join("\n",FileUtils.readLines(resource.getFile(), "UTF-8"));
         System.out.println(content);
         stubFor(get(urlPathEqualTo(url)).willReturn(aResponse().withBody(content).withStatus(200)));
     }
